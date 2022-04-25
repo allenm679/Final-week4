ff233701e80e0411df4cd2ec781d2df98c839a36
Allen Mounivong <102091039+allenm679@users.noreply.github.com>
2022-04-24 23:24:39 -0500

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,12 +1,42 @@
 let pokemonLink = require("./db.json")
+let globalId = pokemon[pokemon.length - 1]. id + 1
 
 module.exports = {
-    pokemon: (req, res) => {
+    getPokemon: (req, res) => {
         res.status(200).send(pokemonLink)
     },
     deletePokemon: (req,res) => {
-        let index = movies.findIndex(elem => elem.id === + req.params.id)
+        const {id} = req.params
+        let index = pokemon.findIndex(poke => poke.id === +id)
         pokemon.splice(index, 1)
         res.status(200).send(pokemonLink)
+    },
+    addPokemon: (req,res) => {
+        const{pokemon, type, color, size} = req.body
+        let newPokemon = {
+            pokemon,
+            type,
+            color,
+            size,
+            id: globalId
+        }
+            pokemon.push(newPokemon)
+            res.status(200).send(pokemonLink)
+            globalId++
+    },
+    updatePokemon: (req,res) => {
+        const {id} = req.params
+        const{pokemon,type,color,size} = req.body
+        let updatePoke = {
+            pokemon,
+            type,
+            color,
+            size,
+            id
+        }
+        let index = pokemon.findIndex(poke=> poke.id === +id)
+        pokemon.splice(index, 1, updatePoke)
+        res.status(200).send(pokemon)
     }
+       
 }
